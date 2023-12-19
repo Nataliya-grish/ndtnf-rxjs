66ea2aea6e9fdba5ced2d34f5a7f0d1b3955c6f0
Nataliya-grish <gray-wings@yandex.ru>
2023-12-19 19:08:26 +0300

    done

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { ajax } from 'rxjs/ajax';
+import { of, from, timer, range } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
-const data$ = ajax.getJSON('https://api.github.com/search/repositories?q=rxjs');
+const o = range(0, 10);
 
-data$.subscribe((value) => console.log('data$ value', value));
+o.subscribe({
+  next: (value: any) => console.log("Next:", value),
+  complete: () => console.log("Complete!"),
+  error: (error) => console.log("Error!", error),
+});
 
-const dataGitLab$ = ajax.getJSON('https://gitlab.com/api/v4/projects?search=nodejs');
+const gitHub = ajax.getJSON("https://api.github.com/search/repositories?q=rxjs"
+);
+gitHub.subscribe((value) => console.log("Github", value));
 
-dataGitLab$.subscribe((value) => console.log('dataGitLab$ value', value));
+const gitLab = ajax.getJSON("https://gitlab.com/api/v4/projects?search=rxjs");
+gitLab.subscribe((value) => console.log("Gitlab", value));
